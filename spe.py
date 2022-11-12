e796d7fc78847a2e69f6d82f2f77283f04b855c5
Daniela Hradilova <hradidan@fel.cvut.cz>
2022-11-12 15:49:10 +0100

    Implementation extended, fastText warning muted, vector dimension as input para, code comments added

--- spe.py
+++ spe.py
@@ -1,31 +1,73 @@
 import numpy as np
 import fasttext
-from scipy import spatial
 import os
 
-def spe(input_sentences):
-    my_path = "classifiers"
-    classifiers = []
-    classifier_files = [f for f in os.listdir(my_path) if os.path.isfile(os.path.join(my_path, f)) and f.endswith(".ftz")]
-    for name in classifier_files:
-        classifiers.append(
-            fasttext.load_model(my_path+ "/" + name)
-        )
+# TODO edge cases - does not throw
 
-    averaged_vector_arr = np.zeros(shape=(len(classifiers), 10))
 
+def spe_sentence(input_sentence, classifiers, vector_dimension):
+    """
+    Processes the input sentence for each classifier into an averaged vector of given dimensions.
+    This process is described in detail in paper https://arxiv.org/abs/2211.04205.
+
+    :param input_sentence: sentence as a string
+    :param classifiers: list of loaded classifiers
+    :param vector_dimension: dimension of the averaged output vector as int
+    :return: averaged output vector for given sentence
+    """
+    averaged_vector_arr = np.zeros(shape=(len(classifiers), vector_dimension))
     # get all vectors from classifiers and put them in an array
     for i, model in enumerate(classifiers):
-        sentence_vector = model.get_sentence_vector(input_sentences)
+        sentence_vector = model.get_sentence_vector(input_sentence)
         averaged_vector_arr[i] = sentence_vector
 
     final_average_vector = averaged_vector_arr.mean(axis=0)
-
     return final_average_vector
 
 
+def load_classifiers():
+    """
+    Loads all the fastText classifiers that are in classifiers_folder_path.
+
+    :return: list of loaded fastText classifiers
+    """
+    classifiers_folder_path = "classifiers"
+    classifiers = []
+    classifier_files = [f for f in os.listdir(classifiers_folder_path) if
+                        os.path.isfile(os.path.join(classifiers_folder_path, f)) and f.endswith(".ftz")]
+    try:
+        # silence the deprecation warnings as the package does not properly use the python 'warnings' package
+        # see https://github.com/facebookresearch/fastText/issues/1056
+        fasttext.FastText.eprint = lambda *args, **kwargs: None
+
+        for name in classifier_files:
+            classifiers.append(
+                fasttext.load_model(classifiers_folder_path + "/" + name)
+            )
+    except:
+        pass
+
+    return classifiers
+
+
+def spe(input_sentences, vector_dimension = 10):
+    """
+    Processes the list of input sentences into a corresponding array of vectors.
+
+    :param input_sentences: list of input sentences as list of strings
+    :param vector_dimension (optional): dimension of the output sentence vectors as int, default value is 10
+    :return: array of vector sentences, one for each sentence
+    """
+    classifiers = load_classifiers()
+    vector_sentences = []
+    for sentence in input_sentences:
+        vector_sentence = spe_sentence(sentence, classifiers, vector_dimension)
+        vector_sentences.append(vector_sentence)
+    return vector_sentences
+
+
 if __name__ == '__main__':
-    input_sentences = "bla bla"
+    input_sentences = ["bla bla", "test"]
     output = spe(input_sentences)
     print("SPE output:")
     print(output)
