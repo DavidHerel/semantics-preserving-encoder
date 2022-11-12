213659f44dff22577346e716b424c4d9729871b9
DavidHerel <david.herel@seznam.cz>
2022-11-12 19:36:22 +0100

    add desc for pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="spe-encoder",
-    version="0.0.1",
+    version="0.0.2",
     author="David Herel, Hugo Cisneros, Daniela Hradilova, Tomas Mikolov",
     description="Sentence embedding technique for textual adversarial attacks",
     packages=find_packages(include=['spe'], exclude=['projects', ]),
+    long_description=readme,
+    long_description_content_type='text/markdown',
     install_requires=['numpy', 'fasttext==0.9.2'],
     classifiers=[
         "Programming Language :: Python :: 3",
