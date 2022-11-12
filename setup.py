c3159c1cd291dc6b3c06d09f5e3a04ec06112c9a
David Herel <david.herel@seznam.cz>
2022-11-12 19:26:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="David Herel, Hugo Cisneros, Daniela Hradilova, Tomas Mikolov",
     description="Sentence embedding technique for textual adversarial attacks",
     packages=find_packages(include=['spe'], exclude=['projects', ]),
-    install_requires=['numpy', 'fasttext'],
+    install_requires=['numpy', 'fasttext==0.9.2'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
